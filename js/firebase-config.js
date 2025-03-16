b2603728e3dbf95001b769ef284aa773621c40be
Ninjaprime875 <140179811+Ninjaprime875@users.noreply.github.com>
2025-03-16 15:24:25 -0400

    visitor counter

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-ZjfaSoH3sNbw798uUSE6euCIACsB7Wk",
+    authDomain: "visitorcounterysi.firebaseapp.com",
+    databaseURL: "https://visitorcounterysi-default-rtdb.firebaseio.com",
+    projectId: "visitorcounterysi",
+    storageBucket: "visitorcounterysi.firebasestorage.app",
+    messagingSenderId: "567057900795",
+    appId: "1:567057900795:web:07b4ee98188f6f52e73c5e",
+    measurementId: "G-LZBGLB3FK9"
+  };
